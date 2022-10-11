4e9e4e241678052bb5c69523afe8e55b0b3cd43a
Dan Postolache <dan_postolache@epam.com>
2022-10-11 13:41:18 +0300

    added third test

--- src/test/java/org/example/DeliverTo.java
+++ src/test/java/org/example/DeliverTo.java
@@ -1,8 +1,6 @@
 package org.example;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +10,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 public class DeliverTo {
     private WebDriver driver;
@@ -25,6 +25,10 @@ public class DeliverTo {
     private final By countriesDropDown = By.xpath("//span[@data-action='a-dropdown-button']");
     private final By location = By.xpath("//span[contains(text(), 'Los Angeles 90001')]");
     private final By poland = By.xpath("//a[@data-value='{\"stringVal\":\"PL\"}']");
+    private final By doneBtn = By.xpath("//button[@name='glowDoneButton']");
+    private final By categories = By.xpath("//h2[text()='Shop by Category' or text()='Gaming accessories']/../following-sibling::div//div[contains(@class, 'quadrant-container')] | //img[@alt='Electronics']");
+    private final By searchResults = By.xpath("//span[@class='a-size-base-plus a-color-base a-text-normal' or @class='a-size-medium a-color-base a-text-normal']");
+    private final By shipToCountry = By.xpath("//span[contains(text(), 'No Import')] | //div[@id='contextualIngressPtLabel_deliveryShortLine']/span[2]");
 
     @BeforeClass
     private void setDriver() {
@@ -56,20 +60,54 @@ public class DeliverTo {
         Assert.assertEquals(getElement(poland).getText(), "Poland");
     }
 
-    private void openPage(String url) {
-        driver.get(url);
+    @Test
+    private void testChangingDeliverToCountry() {
+        openPage(AMAZON_URL);
+        click(deliverToIcon);
+        click(countriesDropDown);
+        click(poland);
+        click(doneBtn);
+        waitForPageRefresh();
+        clickRandomElement(categories);
+        clickRandomElement(searchResults);
+        Assert.assertTrue(getElement(shipToCountry).getAttribute("textContent").contains("Poland"));
     }
 
-    private void click(By locator) {
-        getElement(locator).click();
+    private WebElement getElement(By locator) {
+        return new WebDriverWait(driver, TIMEOUT)
+                .until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    private List<WebElement> getElements(By locator) {
+        return new WebDriverWait(driver, TIMEOUT)
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+    }
+
+    private WebElement getRandomElement(By locator) {
+        List<WebElement> categories = getElements(locator);
+        return getElements(locator).get(new Random().nextInt(categories.size()));
+    }
+
+    private void openPage(String url) {
+        driver.get(url);
     }
 
     private void sendKeys(By locator, String keys) {
         getElement(locator).sendKeys(keys);
     }
 
-    private WebElement getElement(By locator) {
-        return new WebDriverWait(driver, TIMEOUT)
-                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+    private void click(By locator) {
+        getElement(locator).click();
+    }
+
+    private void clickRandomElement(By locator) {
+        new WebDriverWait(driver, TIMEOUT)
+                .until(ExpectedConditions.elementToBeClickable(getRandomElement(locator)))
+                .click();
+    }
+
+    private void waitForPageRefresh() {
+        new WebDriverWait(driver, TIMEOUT)
+                .until(ExpectedConditions.stalenessOf(getElement(deliverToIcon)));
     }
 }
