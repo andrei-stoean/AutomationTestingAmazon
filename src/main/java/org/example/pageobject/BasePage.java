abd4cabc0b27d0aab4a3a39f079d182aed34a6a3
Andrei Stoean <andrei_stoean@epam.com>
2022-10-17 14:18:08 +0300

    Created common pageobjects

---
+++ src/main/java/org/example/pageobject/BasePage.java
@@ -0,0 +1,13 @@
+package org.example.pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+    protected WebDriver webDriver;
+
+    public BasePage(WebDriver webDriver){
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+    }
+}
---
+++ src/main/java/org/example/pageobject/pages/HomePage.java
@@ -0,0 +1,11 @@
+package org.example.pageobject.pages;
+
+import org.example.pageobject.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+}
